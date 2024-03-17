25a8125fa3e656d1dbd148d44204268c3fc5c5fd
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-03-17 15:53:39 +0530

    updates

--- connect/src/ApiConfig/index.js
+++ connect/src/ApiConfig/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://facebook-react-clone-ovj4.onrender.com",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://connect-a45c.onrender.com",
+  // baseURL: "http://localhost:8000",
 });
 
 export default api;
