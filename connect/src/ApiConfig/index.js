0bb30a383bdda1154a5b1c341b7830633ddf8f6b
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-03-26 23:10:47 +0530

    updates

--- connect/src/ApiConfig/index.js
+++ connect/src/ApiConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://connect-a45c.onrender.com",
-  // baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000""",
 });
 
 export default api;
